0cc02f33e61ab958e137a980fa7bd32c123535b7
PrashantNarahatti <91308793+PrashantNarahatti@users.noreply.github.com>
2021-11-13 15:53:11 +0530

    Add files via upload

---
+++ SquareStarMatrix.java
@@ -0,0 +1,23 @@
+class SquareStarMatrix {
+
+	void increaseStarTriangleMatrix() {
+
+		for (int row = 1; row <= 5; row++) {
+			for (int col = 1; col <= 5; col++) {
+
+				System.out.print("*");
+
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+
+		SquareStarMatrix matrix = new SquareStarMatrix();
+
+		matrix.increaseStarTriangleMatrix();
+
+	}
+
+}
